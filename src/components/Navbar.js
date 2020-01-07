a14ad8cfdb6da0289aa25a959bf036c7c9f6bd5e
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2020-01-07 18:04:02 -0500

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,7 @@ import { Nav} from 'react-bootstrap';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import $ from 'jquery';
-import Popper from 'popper.js';
+
 
 const Navbar = () => {
     return (
